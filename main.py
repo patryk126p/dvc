ae96e3a80717d10955f8174746ae2dae7f8ab541
Patryk Pilarski <1patryk.pilarski@gmail.com>
2021-08-14 15:49:38 +0200

    add dummy code

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import joblib
+from sklearn.tree import DecisionTreeClassifier
+
+if __name__ == '__main__':
+    data = pd.read_csv("data/iris.data",
+                       header=None,
+                       names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species'])
+    x = data[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']]
+    y = data.species
+
+    model = DecisionTreeClassifier(max_depth=3, random_state=1)
+    model.fit(x,y)
+    joblib.dump(model, "models/model.joblib")
